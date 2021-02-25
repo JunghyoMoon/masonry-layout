b4ee1887667c156ffe99f2034220141050bfdc7c
JungHyoMoon <dsinhome70@gmail.com>
2021-02-25 22:41:53 +0900

    gulp setup is done

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,5 @@
 // task
-import gulp, { watch } from "gulp";
+import gulp from "gulp";
 import del from "del";
 import server from "gulp-webserver";
 // sass
@@ -9,26 +9,56 @@ import minify from "gulp-csso";
 // js
 import babelify from "babelify";
 import browserify from "gulp-bro";
-import uglify from "uglifyify";
 
 sass.compiler = require("node-sass");
 
 const routes = {
     scss: {
-        src: "./src/scss/**/*.scss",
+        src: "./src/scss/style.scss",
         dest: "./dest/css",
-        watch: "",
+        watch: "./src/scss/**/*.scss",
     },
     js: {
         src: "./src/js/main.js",
         dest: "./dest/js",
-        watch: "",
+        watch: "./src/js/**/*.js",
     },
     img: {},
 };
 
-const prepare = gulp.series([]);
-const assets = gulp.series([]);
-const live = gulp.parallel([]);
+const clean = () => del(["dest"]);
+
+const liveServer = () =>
+    gulp.src("dest").pipe(server({ livereload: true, open: true }));
+
+const js = () =>
+    gulp
+        .src(routes.js.src)
+        .pipe(
+            browserify({
+                transform: [
+                    babelify.configure({ presets: ["@babel/preset-env"] }),
+                    ["uglifyify", { global: true }],
+                ],
+            })
+        )
+        .pipe(gulp.dest(routes.js.dest));
+
+const style = () =>
+    gulp
+        .src(routes.scss.src)
+        .pipe(sass().on("error", sass.logError))
+        .pipe(autoPrefixer())
+        .pipe(minify())
+        .pipe(gulp.dest(routes.scss.dest));
+
+const watch = () => {
+    gulp.watch(routes.scss.watch, style);
+    gulp.watch(routes.js.src, js);
+};
+
+const prepare = gulp.series([clean]);
+const assets = gulp.series([style, js]);
+const live = gulp.parallel([liveServer, watch]);
 
 export const dev = gulp.series([prepare, assets, live]);
