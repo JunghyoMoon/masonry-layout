be4886d61805c6a808422888bc6b547ba7a60338
junghyomoon <dsinhome70@gmail.com>
2021-02-25 23:15:12 +0900

    basic setup is done.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -13,48 +13,48 @@ import browserify from "gulp-bro";
 sass.compiler = require("node-sass");
 
 const routes = {
-    scss: {
-        src: "./src/scss/style.scss",
-        dest: "./dest/css",
-        watch: "./src/scss/**/*.scss",
-    },
-    js: {
-        src: "./src/js/main.js",
-        dest: "./dest/js",
-        watch: "./src/js/**/*.js",
-    },
-    img: {},
+	scss: {
+		src: "src/scss/style.scss",
+		dest: "dest/css",
+		watch: "src/scss/**/*.scss",
+	},
+	js: {
+		src: "src/js/main.js",
+		dest: "dest/js",
+		watch: "src/js/**/*.js",
+	},
+	img: {},
 };
 
-const clean = () => del(["dest"]);
+const clean = () => del(["dest/**/*", "!dest/index.html"]);
 
 const liveServer = () =>
-    gulp.src("dest").pipe(server({ livereload: true, open: true }));
+	gulp.src("dest").pipe(server({ livereload: true, open: true }));
 
 const js = () =>
-    gulp
-        .src(routes.js.src)
-        .pipe(
-            browserify({
-                transform: [
-                    babelify.configure({ presets: ["@babel/preset-env"] }),
-                    ["uglifyify", { global: true }],
-                ],
-            })
-        )
-        .pipe(gulp.dest(routes.js.dest));
+	gulp
+		.src(routes.js.src)
+		.pipe(
+			browserify({
+				transform: [
+					babelify.configure({ presets: ["@babel/preset-env"] }),
+					["uglifyify", { global: true }],
+				],
+			})
+		)
+		.pipe(gulp.dest(routes.js.dest));
 
 const style = () =>
-    gulp
-        .src(routes.scss.src)
-        .pipe(sass().on("error", sass.logError))
-        .pipe(autoPrefixer())
-        .pipe(minify())
-        .pipe(gulp.dest(routes.scss.dest));
+	gulp
+		.src(routes.scss.src)
+		.pipe(sass().on("error", sass.logError))
+		.pipe(autoPrefixer())
+		.pipe(minify())
+		.pipe(gulp.dest(routes.scss.dest));
 
 const watch = () => {
-    gulp.watch(routes.scss.watch, style);
-    gulp.watch(routes.js.src, js);
+	gulp.watch(routes.scss.watch, style);
+	gulp.watch(routes.js.src, js);
 };
 
 const prepare = gulp.series([clean]);
