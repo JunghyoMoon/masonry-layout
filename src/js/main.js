45c9ed4c85777fe5bc0b703f832e93ac44bafdf5
JungHyoMoon <dsinhome70@gmail.com>
2021-02-28 19:33:04 +0900

    there is a bug

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,32 @@
 const wrapper = document.querySelector(".masonry");
 const items = wrapper.querySelectorAll(".item");
 
+const closeModal = (event) => {
+    const modal = event.target.parentNode;
+    console.log(modal);
+};
+
+const getClose = () => {
+    const closeBtn = modal.querySelector(".close");
+    closeBtn.addEventListener("click", closeModal);
+};
+
 function handleClick() {
-	const content = this.innerHTML;
+    const content = this.innerHTML;
+    const modal = document.createElement("div");
+    modal.classList.add("modal");
+    modal.innerHTML = `
+		<button class="close">❌</button>
+		${content}
+	`;
+    wrapper.append(modal);
+    getClose();
 }
 
 const init = () => {
-	items.forEach((item) => item.addEventListener("click", handleClick));
+    items.forEach((item) => item.addEventListener("click", handleClick));
 };
 
 if (wrapper) {
-	init();
+    init();
 }
